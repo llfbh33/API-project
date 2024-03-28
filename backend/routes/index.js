a75ad9837a378aa69eb1c50d634012ae9623eeb4
Aubrie <aubriewoodbine@yahoo.com>
2024-03-28 18:11:22 -0500

    Changed the route for the token cookie for live server

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,18 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
-// server and cookie test
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+// server and cookie test - This test works when used for the live environment
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+router.get("/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
 });
 
 // Add a XSRF-TOKEN cookie
