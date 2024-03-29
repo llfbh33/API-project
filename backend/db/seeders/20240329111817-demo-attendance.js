24eaeb63cd18347754dd433c9d01abb3eaa1146d
Aubrie <aubriewoodbine@yahoo.com>
2024-03-29 06:26:54 -0500

    Add seder for Attendance

---
+++ backend/db/seeders/20240329111817-demo-attendance.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+const { Attendance } = require('../models');
+const { Op } = require('sequelize');
+options.tableName = "Attendances"
+
+const seedAttendances = [
+  {
+    eventId: 1,
+    userId: 1,
+    status: "attending"
+  },
+  {
+    eventId: 2,
+    userId: 1,
+    status: "attending"
+  },
+  {
+    eventId: 1,
+    userId: 2,
+    status: "attending"
+  },
+  {
+    eventId: 1,
+    userId: 3,
+    status: "can't make it"
+  },
+  {
+    eventId: 3,
+    userId: 5,
+    status: "attending"
+  },
+  {
+    eventId: 3,
+    userId: 4,
+    status: "attending"
+  },
+]
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    options.validate = true;
+    await Attendance.bulkCreate(seedAttendances, options);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete(options, seedAttendances);
+  }
+};
