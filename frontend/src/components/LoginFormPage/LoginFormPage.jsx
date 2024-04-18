3650015d14a1c3960a1ec13fcfe75af8e4342529
Aubrie <aubriewoodbine@yahoo.com>
2024-04-18 10:20:51 -0500

    tests

--- frontend/src/components/LoginFormPage/LoginFormPage.jsx
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -29,7 +29,7 @@ function LoginFormPage() {
       <h1>Log In</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          Username or Email
+          Username or Email or something
           <input
             type="text"
             value={credential}
